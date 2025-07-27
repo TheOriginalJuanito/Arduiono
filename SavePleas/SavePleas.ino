6c4bca16088d74558df194a96d48d63cc6fd94ee
TheOriginalJuanito <jon.m.bisaz@gmail.com>
2025-07-27 22:38:36 +0200

    deam

--- SavePleas/SavePleas.ino
+++ SavePleas/SavePleas.ino
@@ -16,23 +16,34 @@ unsigned long lastTime = 0;  // For timing if you need it
 #define SERVO_WRIST_CHANNEL 1
 #define SERVO_ELBOW_CHANNEL 2
 #define SERVO_UPPER_ELBOW_CHANNEL 3
+#define SERVO_SHOULDER1_CHANNEL 4
+#define SERVO_SHOULDER2_CHANNEL 5
 
 // Current servo angles for smooth movement
-int flexSensorHandAngle = 90;
-int flexSensorWristAngle = 120;
-int flexSensorElbowAngle = 90;
-int GYROUpperElbowAngle = 90;
+int flexSensorHandAngle = 0;
+int flexSensorWristAngle = 0;
+int flexSensorElbowAngle = 0;
+int GYROUpperElbowAngle = 0;
+int GYRORollShoulder1Angle = 90;
+int GYRORollShoulder2Angle = 90;
+
 
 // Flex Sensor Pins
 const int flexSensorHandPin = A0;
 const int flexSensorWristPin = A1;
 const int flexSensorElbowPin = A2;
 
+
 // MPU6050 Complementary Filter Variables
 float pitch = 0;       // Filtered pitch angle
 float accelPitch = 0;  // Raw pitch from accelerometer
 float gyroY = 0;       // Raw gyro Y-axis data
 float alpha = 0.96;    // Complementary filter coefficient (0.96 = 96% gyro, 4% accel)
+
+float roll = 0;
+float accelRoll = 0;
+float gyroX = 0;
+
 unsigned long lastTimeMPU = 0;
 
 // Movement smoothing
@@ -82,12 +93,30 @@ void loop() {
   int16_t ax, ay, az, gx, gy, gz;
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
+  // --- Time delta calculation for gyro integration ---
+  float dt = (micros() - lastTimeMPU) / 1000000.0;  // Delta time in seconds
+  lastTimeMPU = micros();
+
+  // --- Complementary Filter for Roll ---
+  // 1. Calculate raw accelerometer roll (degrees)
+  accelRoll = atan2(ay, az) * 180.0 / PI;
+
+  // 2. Get gyro X-axis data (convert to degrees/sec)
+  gyroX = gx / 131.0;
+
+  // 3. Complementary filter: Combine gyro + accelerometer
+  roll = alpha * (roll + gyroX * dt) + (1 - alpha) * accelRoll;
+
+  // --- Complementary Filter for Pitch ---
+  accelPitch = atan2(-ax, sqrt((long)ay * ay + (long)az * az)) * 180.0 / PI;
+  gyroY = gy / 131.0;
+  pitch = alpha * (pitch + gyroY * dt) + (1 - alpha) * accelPitch;
+
   // --- Complementary Filter for Pitch ---
   // 1. Calculate raw accelerometer pitch (degrees)
   accelPitch = atan2(-ax, sqrt((long)ay * ay + (long)az * az)) * 180.0 / PI;
 
   // 2. Get gyro Y-axis data (convert to degrees/sec)
-  float dt = (micros() - lastTimeMPU) / 1000000.0;  // Delta time in seconds
   lastTimeMPU = micros();
   gyroY = gy / 131.0;  // MPU6050 sensitivity factor for +/-250deg/s range
 
@@ -98,14 +127,19 @@ void loop() {
   int flexSensorHandValue = smoothFlexRead(flexSensorHandPin);
   int flexSensorWristValue = smoothFlexRead(flexSensorWristPin);
   int flexSensorElbowValue = smoothFlexRead(flexSensorElbowPin);
+  
 
   // Map flex sensor values to servo angles
   int targetHandAngle = map(flexSensorHandValue, 600, 900, 0, 180);
   int targetWristAngle = map(flexSensorWristValue, 600, 900, 100, 140);
-  int targetElbowAngle = map(flexSensorElbowValue, 600, 900, 30, 150);
+  int targetElbowAngle = map(flexSensorElbowValue, 600, 2000, 30, 150);
 
   // Map filtered pitch to upper elbow angle (adjusted range for natural movement)
   int targetUpperElbowAngle = map(pitch, -45, 45, 60, 120);  // Reduced range for stability
+
+  int targetShoulder1Angle = map(roll, -45, 45, 60, 120);  // Adjust range as needed
+  int targetShoulder2Angle = map(roll, -45, 45, 120, 60);  // Inverse movement
+
 
   // Constrain all angles
   targetHandAngle = constrain(targetHandAngle, 0, 180);
@@ -113,23 +147,33 @@ void loop() {
   targetElbowAngle = constrain(targetElbowAngle, 0, 180);
   targetUpperElbowAngle = constrain(targetUpperElbowAngle, 0, 180);
 
+  targetShoulder1Angle = constrain(targetShoulder1Angle, 0, 180);
+  targetShoulder2Angle = constrain(targetShoulder2Angle, 0, 180);
+
   // Smoothly move servos toward targets
   flexSensorHandAngle += constrain(targetHandAngle - flexSensorHandAngle, -stepSize, stepSize);
   flexSensorWristAngle += constrain(targetWristAngle - flexSensorWristAngle, -stepSize, stepSize);
   flexSensorElbowAngle += constrain(targetElbowAngle - flexSensorElbowAngle, -stepSize, stepSize);
   GYROUpperElbowAngle += constrain(targetUpperElbowAngle - GYROUpperElbowAngle, -stepSize, stepSize);
+  GYRORollShoulder1Angle += constrain(targetShoulder1Angle - GYRORollShoulder1Angle, -stepSize, stepSize);
+  GYRORollShoulder2Angle += constrain(targetShoulder2Angle - GYRORollShoulder2Angle, -stepSize, stepSize);
 
   // Clamp angles
   flexSensorHandAngle = constrain(flexSensorHandAngle, 0, 180);
   flexSensorWristAngle = constrain(flexSensorWristAngle, 0, 180);
   flexSensorElbowAngle = constrain(flexSensorElbowAngle, 0, 180);
   GYROUpperElbowAngle = constrain(GYROUpperElbowAngle, 0, 180);
+  GYRORollShoulder1Angle = constrain(GYRORollShoulder1Angle, 0, 180);
+  GYRORollShoulder2Angle = constrain(GYRORollShoulder2Angle, 0, 180);
 
   // Drive servos
   pwm.setPWM(SERVO_HAND_CHANNEL, 0, angleToPulse(flexSensorHandAngle));
   pwm.setPWM(SERVO_WRIST_CHANNEL, 0, angleToPulse(flexSensorWristAngle));
   pwm.setPWM(SERVO_ELBOW_CHANNEL, 0, angleToPulse(flexSensorElbowAngle));
   pwm.setPWM(SERVO_UPPER_ELBOW_CHANNEL, 0, angleToPulse(GYROUpperElbowAngle));
+  pwm.setPWM(SERVO_SHOULDER1_CHANNEL, 0, angleToPulse(GYRORollShoulder1Angle));
+  pwm.setPWM(SERVO_SHOULDER2_CHANNEL, 0, angleToPulse(GYRORollShoulder2Angle));
+
 
   delay(delayPerStep);
 }
